14fc92bca8c532855a72d7cc600a7f3ca929eb4d
Alex Pic <alex.pic.java@gmail.com>
2022-06-02 18:06:07 +0200

    # Commit v0.2 Added ArrayList

--- part06_2/src/com/company/UserInterface.java
+++ part06_2/src/com/company/UserInterface.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInterface {
 
     private Scanner scanner;
+    private ArrayList<String> words;
 
     public UserInterface() {
         this.scanner = new Scanner(System.in);
+        this.words = new ArrayList<>();
     }
 
     public void start() {
@@ -19,16 +22,16 @@ public class UserInterface {
             if (alreadyEntered(word)) {
                 break;
             }
+            this.words.add(word);
         }
 
         System.out.println("You gave the same word twice!");
     }
 
     public boolean alreadyEntered(String word) {
-        if (word.equals("end")) {
+        if (this.words.contains(word)) {
             return true;
         }
-
         return false;
     }
 
