05613c21cbdc41ca463009f941c07dc593f1cc3b
Alex Pic <alex.pic.java@gmail.com>
2022-06-03 15:53:00 +0200

    # Commit v.2.6 Updated to released

--- part06_02_OneToMany/src/Program.java
+++ part06_02_OneToMany/src/Program.java
@@ -7,35 +7,7 @@ public class Program {
 
         GradeRegister register = new GradeRegister();
 
-        while (true) {
-            System.out.print("Points: ");
-            String input = scanner.nextLine();
-            if (input.equals("")) {
-                break;
-            }
-
-            int score = Integer.valueOf(input);
-
-            if (score < 0 || score > 100) {
-                System.out.println("Impossible number.");
-                continue;
-            }
-
-            register.addGradeBasedOnPoints(score);
-        }
-
-        System.out.println("");
-        int grade = 5;
-        while (grade >= 0) {
-            int stars = register.numberOfGrades(grade);
-            System.out.print(grade + ": ");
-            while (stars > 0) {
-                System.out.print("*");
-                stars--;
-            }
-            System.out.println("");
-
-            grade = grade - 1;
-        }
+        UserInterface userInterface = new UserInterface(register, scanner);
+        userInterface.start();
     }
 }
