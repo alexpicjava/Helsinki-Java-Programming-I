ecae3e63a8c8eae12b3edc92b3081c1bb2ffd7b8
alexPic <alex.pic.java@gmail.com>
2022-06-06 22:13:11 +0200

    # Commit v 3.2 Updated class Calculator

--- part06_03_Calculator/src/Claculator.java
+++ part06_03_Calculator/src/Claculator.java
@@ -1,4 +1,17 @@
 public class Claculator {
 
+    private int value;
 
+    public Claculator(int value) {
+        this.value = value;
+    }
+    public void add(int number){
+        this.value = this.value + number;
+    }
+    public void subtract(int number){
+        this.value = this.value + number;
+    }
+    public int getValue(){
+        return this.value;
+    }
 }
