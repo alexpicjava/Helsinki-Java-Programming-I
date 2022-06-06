69e1bbd8e07f535d0134a41ac4c3a798fc119901
alexPic <alex.pic.java@gmail.com>
2022-06-06 23:03:47 +0200

    # Commit v 3.7 Two new Test Methods added in CalculatorTest.

--- part06_03_Calculator/test/CalculatorTest.java
+++ part06_03_Calculator/test/CalculatorTest.java
@@ -8,5 +8,18 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(0, calculator.getValue());
     }
+    @Test
+    public void valueFiveWhenFiveAdded() {
+        Calculator calculator = new Calculator();
+        calculator.add(5);
+        assertEquals(5, calculator.getValue());
+    }
+
+    @Test
+    public void valueMinusTwoWhenTwoSubstracted() {
+        Calculator calculator = new Calculator();
+        calculator.subtract(2);
+        assertEquals(-2, calculator.getValue());
+    }
 
 }
